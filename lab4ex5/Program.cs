8dcc82b80462363a3056fae3988945cc56ec6b9f
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-17 20:55:02 +0300

    Add project files.

---
+++ lab4ex5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n, apelati-o si afisati-i rezultatul.  .*/
+
+
+            Console.Write("Numarul n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Suma numerelor de la 1 la " + n + " " + "este: ");
+            Console.WriteLine(SumaN(n));
+        }
+
+        static int SumaN(int n)
+        {
+            if (n <= 0)
+            {
+                return n;
+            }
+            return n + SumaN(n - 1);
+
+        }
+    }
+}
